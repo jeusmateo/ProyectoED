5342d41ea1e894f774f5d108beb07feae6f4646b
jeusmatthew <jeusmateo@gmail.com>
2024-04-26 00:17:11 -0600

    shellsort directito con arreglo primitivo String[]

---
+++ src/ordenamiento/ShellSortString.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ordenamiento;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author jeusm
+ */
+public class ShellSortString {
+    String[] A;
+
+    public ShellSortString(String[] A) {
+        this.A = A;
+    }
+
+    public void sort() {
+        int intervalo, i;
+        String aux;
+        boolean band;
+        intervalo = A.length;
+        while (intervalo > 1) {
+            intervalo /= 2;
+            band = true;
+            while (band) {
+                band = false;
+                i = 0;
+                while ((intervalo + i) < A.length) {
+                if (A[i].compareTo(A[i+intervalo]) > 0) {
+                        aux = A[i];
+                        A[i] = A[i + intervalo];
+                        A[i + intervalo] = aux;
+                        band = true;
+                    }
+                    i++;
+                }
+            }
+        }
+    }
+}
